dfd279fd590d9f1bfb7ae7ce63631b0fa01c7f11
nicoleleman <nicoman329@outlook.com>
2020-05-08 18:25:28 +0100

    Scraped the slot data, added twilio functionality

--- src/data/get_slot.py
+++ src/data/get_slot.py
@@ -1,4 +1,6 @@
 import requests
+from datetime import datetime
+from twilio.rest import Client
 
 # Read in sensitive info from local file
 secret_dict = {}
@@ -25,6 +27,33 @@ json_payload = {'requestorigin' : 'gi',
                         'service_info' : {'fulfillment_type': "DELIVERY",
                                         'enable_express': 'false'}
                     }}
-
 send_request = requests.post(url, json = json_payload)
-print(send_request.json())
+
+slot_availability = {}
+for slot_day in send_request.json()['data']['slot_days']:
+    slot_date = slot_day['slot_date']
+    for slot in slot_day['slots']:
+        slot_time = slot['slot_info']['start_time']
+        # convert time into datetime object
+        slot_time = datetime.strptime(slot_time, '%Y-%m-%dT%H:%M:%SZ')
+        slot_status = slot['slot_info']['status']
+
+        # write the slot time and the corresponding status to the empty dictionary
+        slot_availability[slot_time.strftime('%H:%M:%S %d-%m-%Y')] = slot_status
+
+
+available_slots = [k for k, v in slot_availability.items() if v != 'UNAVAILABLE']
+
+# if there are available slots, send a text with the slots to my mobile number
+if len(available_slots) > 0:
+    account_sid = secret_dict.get('twilio_account_id')
+    auth_token  = secret_dict.get('twilio_auth_token')
+    client = Client(account_sid, auth_token)
+
+    message = client.messages.create(
+        to=secret_dict.get('my_number'),
+        from_=secret_dict.get('twilio_number'),
+        body="Available delivery slots found: {}".format(available_slots))
+    print(message.sid)
+else:
+    print("No delivery slots are currently available.")
