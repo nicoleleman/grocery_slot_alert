44ccb66e2760202adc587f9955cb3edfb54c80f6
nicoleleman <nicoman329@outlook.com>
2020-05-21 01:04:24 +0100

    Updated environment variables

--- src/data/get_slot.py
+++ src/data/get_slot.py
@@ -1,4 +1,5 @@
 import requests
+import os
 from datetime import datetime
 from twilio.rest import Client
 
@@ -15,15 +16,15 @@ url = 'https://groceries.asda.com/api/v3/slot/view'
 # POST request JSON payload
 json_payload = {'requestorigin' : 'gi',
                     'data' : {
-                        'customer_info' : {'account_id' : secret_dict.get('account_id')},
+                        'customer_info' : {'account_id' : os.environ["ASDA_ACT_ID"]},
                         'order_info' : {'order_id': "20826667453",
                                         'restricted_item_types': [],
                                         'volume': 0,
                                         'weight': 0,
                                         'sub_total_amount': 0},
-                        'service_address' : {'postcode': secret_dict.get('postcode'),
-                                            'latitude': secret_dict.get('latitude'),
-                                            'longitude': secret_dict.get('longitude')},
+                        'service_address' : {'postcode': os.environ["HOME_POSTCODE"],
+                                            'latitude': os.environ["HOME_LAT"],
+                                            'longitude': os.environ["HOME_LON"]},
                         'service_info' : {'fulfillment_type': "DELIVERY",
                                         'enable_express': 'false'}
                     }}
@@ -46,13 +47,13 @@ available_slots = [k for k, v in slot_availability.items() if v != 'UNAVAILABLE'
 
 # if there are available slots, send a text with the slots to my mobile number
 if len(available_slots) > 0:
-    account_sid = secret_dict.get('twilio_account_id')
-    auth_token  = secret_dict.get('twilio_auth_token')
+    account_sid = os.environ["TWILIO_ACT_ID"]
+    auth_token  = os.environ["TWILIO_AUTH_TOKEN"]
     client = Client(account_sid, auth_token)
 
     message = client.messages.create(
-        to=secret_dict.get('my_number'),
-        from_=secret_dict.get('twilio_number'),
+        to=os.environ["MOBILE_NUM"],
+        from_=os.environ["TWILIO_NUM"],
         body="Available delivery slots found: {}".format(available_slots))
     print(message.sid)
 else:
